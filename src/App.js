ae6037009f96f11d56e9de7e13d2eccacf326440
Joao Matheus <joao.matheus2819@gmail.com>
2017-12-22 22:56:26 -0200

    List all people started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import People from './components/People';
+
 class App extends Component {
   render() {
-    return(
-      <div>
-        
-      </div>
+    return (
+      <Router>
+        <div className="App">
+
+          <Route path="/people" exact component={People} />
+          <Route path="/people/:peopleId" exact component={People} />
+
+        </div>
+      </Router>
+
     );
   }
 }
-
 
 export default App;
---
+++ src/components/People.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class People extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: [],
+      next: '',
+      previous: ''
+    }
+  }
+
+  getPeople() {
+    return axios.get('https://swapi.co/api/people')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          people: response.data.results,
+          next: response.data.next,
+          previous: response.data.previous
+        })
+        console.log(this.state.people)
+      });
+  }
+
+  goTo(next, previous) {
+    if (next !== null && previous !== null) {
+      return axios.get(next, previous)
+        .then((response) => {
+          console.log(response.data);
+          this.setState({
+            people: response.data.results,
+            next: response.data.next,
+            previous: response.data.previous
+          })
+        });
+    }
+    console.log("erro")
+  }
+
+  componentDidMount() {
+    this.getPeople();
+  }
+
+  render() {
+    const { people, next, previous } = this.state;
+
+    return (
+      <div className="People">
+        {people.map((peoples, i) =>
+          <div key={i}>
+            <h1>{peoples.name}</h1>
+            <h3>{peoples.height}</h3>
+          </div>
+        )}
+        <button onClick={() => this.goTo(previous)}> <span>Teste 0 </span> </button>
+        <button onClick={() => this.goTo(next)}> <span>Teste 1 </span> </button>
+      </div>
+    );
+  }
+}
+
+export default People;
