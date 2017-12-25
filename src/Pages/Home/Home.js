3d588c0ab792342cd99b8b60633376d76f585ce4
Joao Matheus <joao.matheus2819@gmail.com>
2017-12-25 18:23:08 -0200

    Home structure created

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import LogoStarWars from '../../components/Logo/LogoStarWars';
 import './styles/Home.scss'
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Homeee</h1>
+        <div className="Home-header">
+          <LogoStarWars/>
+        </div>
+        <div className="Home-body">
+          <h1 className="Home-body-title">Personagens</h1>
+          <h1 className="Home-body-title">Planetas</h1>
+        </div>
       </div>
     );
   }
