6d184197b3b4c325a2d3523677b53504f18eaf12
Joao Matheus <joao.matheus2819@gmail.com>
2017-12-25 18:24:11 -0200

    Validation in NextAndPrevious

--- src/components/Pagination/NextAndPrevious.js
+++ src/components/Pagination/NextAndPrevious.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Button from '../Button/Button';
+import React, { Component } from "react";
+import axios from "axios";
+import Button from "../Button/Button";
+import "./styles/NextAndPrevious.scss";
 
 class NextAndPrevious extends Component {
   goTo(next, previous) {
     const { updateData } = this.props;
     if (next !== null && previous !== null) {
-      return axios.get(next, previous)
-        .then((response) => {
-          updateData(response.data)
-        });
+      return axios.get(next, previous).then(response => {
+        updateData(response.data);
+      });
     }
-    console.log("Erro pagina nao encontrada")
+    console.log("Erro pagina nao encontrada");
   }
   render() {
     const { next, previous } = this.props;
-
     return (
       <div className="NextAndPrevious">
-        <Button onClick={() => this.goTo(previous)}>Anterior</Button>
-        <Button onClick={() => this.goTo(next)}>Proximo</Button>
-      </div>    
+        {/* Validação Botão Anterior */}
+        {previous !== null ? (
+          <Button onClick={() => this.goTo(previous)}>Anterior</Button>
+        ) : (
+            <Button className="disable" disabled="disabled">Anterior</Button>
+          )}
+
+        {/* Validação Botão Proximo */}
+        {next !== null ? (
+          <Button onClick={() => this.goTo(next)}>Proximo</Button>
+        ) : (
+            <Button className="disable" disabled="disabled">Proximo</Button>
+          )}
+      </div>
     );
   }
 }
