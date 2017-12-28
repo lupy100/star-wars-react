21286107f1707da2dcb60af93119f475408efc76
Joao Matheus <joao.matheus2819@gmail.com>
2017-12-28 19:50:24 -0200

    Home bug commit fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import LogoStarWars from '../../components/Logo';
 import './styles/Home.scss'
+
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <div className="Home-header">
           <LogoStarWars/>
-        </div>
-        <div className="Home-body">
-          <h1 className="Home-body-title">Characters</h1>
-          <h1 className="Home-body-title">Planets</h1>
         </div>
       </div>
     );
